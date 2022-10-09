e23ff4af415676c7cbef0d3d3591ee5800a80f97
savindu-pasintha <savindupasingtha@gmail.com>
2022-10-09 21:24:09 +0530

    savindu pasintha

--- PrinterApp/Form1.Designer.cs
+++ PrinterApp/Form1.Designer.cs
@@ -108,7 +108,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::ESCPOSThermalPrinterUSBSupport.Properties.Resources.Salesplay_Logo__4;
+            this.pictureBox1.Image = global::ESCPOSThermalPrinterUSBSupport.Properties.Resources.Salesplay_Logo;
             this.pictureBox1.Location = new System.Drawing.Point(58, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(303, 103);
--- PrinterApp/Properties/Resources.Designer.cs
+++ PrinterApp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace ESCPOSThermalPrinterUSBSupport.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Salesplay_Logo {
+            get {
+                object obj = ResourceManager.GetObject("Salesplay_Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Salesplay_Logo__2 {
             get {
                 object obj = ResourceManager.GetObject("Salesplay_Logo__2", resourceCulture);
